0ae86b05c99465c7016ad92eece09396a21f5547
silverblade34 <mpacheco.tacay@gmail.com>
2024-06-17 23:31:56 -0500

    Recibiendo data de producto en detalle producto

--- src/views/DetailProductView.vue
+++ src/views/DetailProductView.vue
@@ -1,19 +1,43 @@
 <template>
-    <div>
-        <h2>Detalle del producto</h2>
+    <div class="w-full flex justify-center py-10">
+        <div class="w-[80%]">
+            <div class="grid grid-cols-3 gap-5">
+                <div>
+                    <img :src="producto1" alt="">
+                </div>
+                <div class="col-span-2">
+                  <p class="text-2xl font-bold">  {{ product.Name }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+import producto1 from '@/assets/products/img1.jpeg';
+import { findAllProductsApi } from "@/api/ProductsService";
 
 export default ({
+    data() {
+        return {
+            producto1,
+        }
+    },
     setup() {
         const route = useRoute();
         const routeParams = ref(route.params);
+        const product = ref({});
+
+        onMounted(async () => {
+            const dataProductsResponse = await findAllProductsApi();
+            console.log(dataProductsResponse.data);
+            product.value = dataProductsResponse.data.find(prod => prod.id == routeParams.value.id)
+        })
 
         return {
-            routeParams
+            routeParams,
+            product
         }
     }
 })
